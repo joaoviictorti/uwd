bbbb46e84f3008bbf4bd4a5b5011b72c485876c3
joaoviictorti <joaovictorti08@gmail.com>
2025-04-26 13:15:34 -0300

    Update tests

--- tests/src/main.rs
+++ tests/src/main.rs
@@ -2,7 +2,7 @@ use dinvk::{GetModuleHandle, GetProcAddress};
 use uwd::spoof;
 
 #[rustfmt::skip]
-fn main() -> Result<(), Box<dyn std::error::Error>> {
+fn main() -> Result<(), Box<dyn core::error::Error>> {
     // Resolves addresses of the WinAPI functions to be used
     let kernel32 = GetModuleHandle("kernel32.dll", None);
     let win_exec = GetProcAddress(kernel32, "WinExec", None);
@@ -13,9 +13,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     // 0:000> x kernel32!WinExec
     // 0:000> bp <addr> "k ; g"
     for _ in 0..50 {
-        spoof!(win_exec, cmd.as_ptr(), 1)
-            .filter(|&ptr| !ptr.is_null())
-            .ok_or("WinExec Failed")?;
+        spoof!(win_exec, cmd.as_ptr(), 1)?;
     }
 
     Ok(())
