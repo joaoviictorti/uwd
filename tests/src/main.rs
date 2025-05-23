609056a3b6037310c205e5ec25641c88eb31bfb5
joaoviictorti <joaovictorti08@gmail.com>
2025-05-23 14:25:12 -0300

    Update tests

--- tests/src/main.rs
+++ tests/src/main.rs
@@ -1,18 +1,32 @@
-use dinvk::{GetModuleHandle, GetProcAddress};
-use uwd::spoof;
-
-#[rustfmt::skip]
-fn main() -> Result<(), Box<dyn core::error::Error>> {
+#[test]
+fn test_win_exec() -> Result<(), Box<dyn core::error::Error>> {
     // Resolves addresses of the WinAPI functions to be used
-    let kernel32 = GetModuleHandle("kernel32.dll", None);
-    let win_exec = GetProcAddress(kernel32, "WinExec", None);
+    let kernel32 = dinvk::GetModuleHandle("kernel32.dll", None);
+    let win_exec = dinvk::GetProcAddress(kernel32, "WinExec", None);
     
     // Execute command with `WinExec`
     let cmd = c"calc.exe";
 
     // 0:000> bp kernel32!WinExec "k ; g"
     for _ in 0..50 {
-        spoof!(win_exec, cmd.as_ptr(), 1)?;
+        uwd::spoof!(win_exec, cmd.as_ptr(), 1)?;
+    }
+
+    Ok(())
+}
+
+#[test]
+fn test_win_exec_synthetic() -> Result<(), Box<dyn core::error::Error>> {
+    // Resolves addresses of the WinAPI functions to be used
+    let kernel32 = dinvk::GetModuleHandle("kernel32.dll", None);
+    let win_exec = dinvk::GetProcAddress(kernel32, "WinExec", None);
+    
+    // Execute command with `WinExec`
+    let cmd = c"calc.exe";
+
+    // 0:000> bp kernel32!WinExec "k ; g"
+    for _ in 0..50 {
+        uwd::spoof_synthetic!(win_exec, cmd.as_ptr(), 1)?;
     }
 
     Ok(())
