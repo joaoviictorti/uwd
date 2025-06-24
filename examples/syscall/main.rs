522e81cd69bc5ef9ccf660afd542a46dd3f0f001
joaoviictorti <joaovictorti08@gmail.com>
2025-06-24 20:10:20 -0300

    fix: Adjusting clippy warning

--- examples/syscall/main.rs
+++ examples/syscall/main.rs
@@ -6,7 +6,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut addr = null_mut::<c_void>();
     let mut size = (1 << 12) as usize;
     let mut status = syscall!("NtAllocateVirtualMemory", -1isize, addr.as_uwd_mut(), 0, size.as_uwd_mut(), 0x3000, 0x04)? as i32;
-    if !(status >= 0) {
+    if status < 0 {
         eprintln!("NtAllocateVirtualMemory Failed With Status: {status:#X}");
         return Ok(());
     }
@@ -17,7 +17,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut addr = null_mut::<c_void>();
     let mut size = (1 << 12) as usize;
     status = syscall_synthetic!("NtAllocateVirtualMemory", -1isize, addr.as_uwd_mut(), 0, size.as_uwd_mut(), 0x3000, 0x04)? as i32;
-    if !(status >= 0) {
+    if status < 0 {
         eprintln!("NtAllocateVirtualMemory Failed With Status [2]: {status:#X}");
         return Ok(());
     }
