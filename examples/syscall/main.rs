394f4887db398d9b0d09506682bd81951b9f1134
joaoviictorti <joaovictorti08@gmail.com>
2025-10-27 20:58:06 -0300

    Update examples

--- examples/syscall/main.rs
+++ examples/syscall/main.rs
@@ -2,7 +2,7 @@ use std::{ffi::c_void, ptr::null_mut};
 use uwd::{AsPointer, syscall};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    // Running indirect syscall with Call Stack Spoofing (Desync)
+    // Running indirect syscall with Call Stack Spoofing
     let mut addr = null_mut::<c_void>();
     let mut size = (1 << 12) as usize;
     let status = syscall!("NtAllocateVirtualMemory", -1isize, addr.as_ptr_mut(), 0, size.as_ptr_mut(), 0x3000, 0x04)? as i32;
