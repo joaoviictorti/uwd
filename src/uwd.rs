4b4212a0038a5c6be1aeefab764bae1278157bb7
joaoviictorti <joaovictorti08@gmail.com>
2025-11-04 23:25:28 -0300

    Add license

--- src/uwd.rs
+++ src/uwd.rs
@@ -1,3 +1,6 @@
+// Copyright (c) 2025 joaoviictorti
+// Licensed under the MIT License. See LICENSE file in the project root for details.
+
 use alloc::{string::String, vec::Vec};
 use core::ffi::c_void;
 
